81d17562ea83d7d93f6e0d41be56b774708c4477
Maksym Panasiuk <mak7devel@gmail.com>
2022-09-09 01:06:28 +0300

    done site without adaptive

---
+++ app/js/script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//     $('.slider').slick({
+//       setting-name: setting-value
+//     });
+// });
+
+$(function(){
+
+    $('.slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+
+});
